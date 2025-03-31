e69dbab2f0be901bcb744fe99c7f04c4e34ad8d5
MandaliaSPG <sofiapg.2002@hotmail.com>
2025-03-31 07:30:26 -0700

    Second Commit

---
+++ src/app.ts
@@ -0,0 +1,19 @@
+import express from 'express'
+import { Envs } from './config/'
+import { GithubController } from './presentation/github/controller'
+
+(()=>{
+    main()
+})()
+
+function main(){
+    const app = express()
+
+    const controller = new GithubController()
+
+    app.post('/api/github',controller.webHookHandler)
+
+    app.listen(Envs.PORT, ()=>{
+        console.log(`App Running on port ${Envs.PORT}`)
+    })
+}
